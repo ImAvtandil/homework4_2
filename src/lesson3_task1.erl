f3b3c65d5c7c20857f3f11fc567ed3f2fbbbaebc
sanr <oleksandr.lishchynskyy@ria.com>
2023-09-09 15:24:21 +0300

    fix utf8

--- src/lesson3_task1.erl
+++ src/lesson3_task1.erl
@@ -6,8 +6,8 @@ first_word(Bitstring) ->
     first_word(Bitstring, <<>>).
 
 first_word(<<S/utf8, 32, _/binary>>, Acc) ->
-    <<Acc/binary, S>>;
+    <<Acc/binary, S/utf8>>;
 first_word(<<S/utf8, T/binary>>, Acc) ->
-    first_word(T, <<Acc/binary, S>>);
+    first_word(T, <<Acc/binary, S/utf8>>);
 first_word(<<>>, Acc) ->
     Acc.
--- src/lesson3_task2.erl
+++ src/lesson3_task2.erl
@@ -6,11 +6,11 @@ words(String) ->
     reverse(words(String, <<>>, [])).
 
 words(<<S/utf8, 32, T/binary>>, AccBin, Acc) ->
-    words(T, <<>>, [<<AccBin/binary, S>> | Acc]);
+    words(T, <<>>, [<<AccBin/binary, S/utf8>> | Acc]);
 words(<<32, T/binary>>, AccBin, Acc) ->
     words(T, AccBin, Acc);
 words(<<S/utf8, T/binary>>, AccBin, Acc) ->
-    words(T, <<AccBin/binary, S>>, Acc);
+    words(T, <<AccBin/binary, S/utf8>>, Acc);
 words(<<>>, AccBin, Acc) ->
     [AccBin | Acc].
 
